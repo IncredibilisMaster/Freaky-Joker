d3f527daa62f074c34f9e7ae4c09cc6633dbe502
IncredibilisMaster <74770590+IncredibilisMaster@users.noreply.github.com>
2025-11-07 17:43:50 +0530

    config.js

--- config.js
+++ config.js
@@ -30,10 +30,11 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "87.106.62.92", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 11642,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "glace", 
-                secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
+                host : nexus.voidhosting.vip
+                port : 6004
+                pass : cocaine 
+                secure : false
+                status : online
             },
             
             /**
@@ -101,4 +102,5 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
